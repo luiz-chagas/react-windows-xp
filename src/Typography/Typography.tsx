b95b835a927712721994a94c05f844317ba676b0
Luiz Chagas <luizchagasjr@gmail.com>
2020-10-30 15:25:54 -0500

    Add textAlign to Typography

--- src/Typography/Typography.tsx
+++ src/Typography/Typography.tsx
@@ -12,31 +12,66 @@ export interface TypographyProps {
     | "h5"
     | "h6"
     | "terminal";
+  alignment?: "left" | "center" | "right";
 }
 
 export const Typography: FunctionComponent<TypographyProps> = ({
   variant,
   children,
+  alignment,
 }) => {
   const className = "winxp-font";
+  const textAlign = alignment ?? "left";
 
   switch (variant) {
     case "span":
-      return <span className={className}>{children}</span>;
+      return (
+        <span className={className} style={{ textAlign }}>
+          {children}
+        </span>
+      );
     case "paragraph":
-      return <p className={className}>{children}</p>;
+      return (
+        <p className={className} style={{ textAlign }}>
+          {children}
+        </p>
+      );
     case "h1":
-      return <h1 className={className}>{children}</h1>;
+      return (
+        <h1 className={className} style={{ textAlign }}>
+          {children}
+        </h1>
+      );
     case "h2":
-      return <h2 className={className}>{children}</h2>;
+      return (
+        <h2 className={className} style={{ textAlign }}>
+          {children}
+        </h2>
+      );
     case "h3":
-      return <h3 className={className}>{children}</h3>;
+      return (
+        <h3 className={className} style={{ textAlign }}>
+          {children}
+        </h3>
+      );
     case "h4":
-      return <h4 className={className}>{children}</h4>;
+      return (
+        <h4 className={className} style={{ textAlign }}>
+          {children}
+        </h4>
+      );
     case "h5":
-      return <h5 className={className}>{children}</h5>;
+      return (
+        <h5 className={className} style={{ textAlign }}>
+          {children}
+        </h5>
+      );
     case "h6":
-      return <h6 className={className}>{children}</h6>;
+      return (
+        <h6 className={className} style={{ textAlign }}>
+          {children}
+        </h6>
+      );
     case "terminal":
       return <pre>{children}</pre>;
   }
